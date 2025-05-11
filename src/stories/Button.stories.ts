81c2c2d630c505f98671421547c983feb119495d
Ashutosh Tiwari <mail@ashutoshtiwari.co.in>
2025-05-11 16:43:06 +0530

    Install shadcn and add button as example

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,53 +1,28 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import type { Meta, StoryObj } from '@storybook/react'
 
-import { Button } from './Button';
+import { Button } from '../components/ui/button'
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
-  title: 'Example/Button',
+const meta: Meta<typeof Button> = {
   component: Button,
+  title: 'Components/Button',
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+}
+export default meta
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof Button>
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    children: 'Click me',
+    variant: 'default',
   },
-};
+}
 
-export const Secondary: Story = {
+export const Outline: Story = {
   args: {
-    label: 'Button',
+    children: 'Outline button',
+    variant: 'outline',
   },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
+}
